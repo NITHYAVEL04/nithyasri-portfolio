2b70f5e5f07b5a78ce77b193edcd953f1451363d
NITHYAVEL04 <nithyavel04@gmail.com>
2026-01-28 15:20:22 +0530

    Connected to live backend

--- server.js
+++ server.js
@@ -11,4 +11,5 @@ const projects = [
 
 app.get('/api/projects', (req, res) => { res.json(projects); });
 
-app.listen(3000, () => console.log('Backend running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
